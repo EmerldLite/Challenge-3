27080b8524f53ea7c1ad7fd8c32e983f3521ca70
Gavin McAllister <55366106+EmerldLite@users.noreply.github.com>
2019-11-17 21:58:39 -0500

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    private int score;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private bool win;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves ());
+    }
+
+    void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.T))
+            {
+                SceneManager.LoadScene("Tutorial 3 WIP"); // or whatever the name of your scene is
+            }
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds (startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range (0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'T' for Restart";
+                restart = true;
+                break;
+            }
+
+            if (win)
+            {
+                restartText.text = "Press 'T' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+
+        if (score >= 100)
+        {
+            gameOverText.text = "You win! Game Created by Gavin McAllister!";
+            win = true;
+            restart = true;
+        }
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over! Game Created by Gavin McAllister!";
+        gameOver = true;
+    }
+}
